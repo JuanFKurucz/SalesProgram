cb798a191a72fd731a9170e4afbcfba3a0280895
JFK PC <juanfkurucz@gmail.com>
2018-01-20 19:19:16 -0300

    opt

--- main.js
+++ main.js
@@ -1,38 +1,6 @@
-const {app,BrowserWindow} = require('electron');
+const electron = require('electron');
 
-let mainWindow;
-
-if (require('electron-squirrel-startup')) {
-	app.quit();
-}
-
-function isWindowsOrmacOS() {
-	return process.platform === 'darwin' || process.platform === 'win32';
-}
-
-
-function init(){
-	mainWindow = new BrowserWindow(
-		{
-			title:"Sales Program",
-			width: 940,
-			height: 640,
-			frame:false
-		}
-	)
-	mainWindow.loadURL(require("path").normalize(__dirname+"\\gui\\index.html"));
-}
-
-app.on('ready', function(){
-	init();
-})
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-app.on('activate', function () {
-  if (mainWindow === null) {
-		init();
-  }
+electron.app.on('ready',function(){
+	let mainWindow = new electron.BrowserWindow({frame:false});
+	mainWindow.loadURL(__dirname+"\\gui\\index.html");
 })
